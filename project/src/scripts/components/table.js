fc21409522a3ec957b7a4df39660feaf6ff0444f
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-01-30 20:26:02 +0100

    moved table class into a separate file

---
+++ project/src/scripts/components/table.js
@@ -0,0 +1,51 @@
+import { createClientRowElement } from '../elementGenerators';
+import ClientsState from '../state/clientsState';
+import api from '../api';
+
+// tableElements is there created rows will be appended to
+// sortConrols is some parent element that has buttons with class sortControlSelector
+// sort button have (data-sort-method-name="sortedById")
+// data-attribute with the name of sorting method of getClientsInterface
+// which will be called on click
+export default class TableApp {
+  constructor({ tableElement, sortControls, sortControlSelector }) {
+    this.table = tableElement;
+    this.sortControls = sortControls;
+    this.sortControlSelector = sortControlSelector;
+  }
+
+  setSortControlsEventListeners() {
+    this.sortControls.addEventListener('click', ({ target }) => {
+      if (target.classList.contains(this.sortControlSelector)) {
+        const { sortMethodName } = target.dataset;
+        const studentInterface = this.state.getClientsInterface();
+        this.clients = studentInterface[sortMethodName]().data();
+        this.render();
+      }
+    });
+  }
+
+  setEventListeners() {
+    this.setSortControlsEventListeners();
+  }
+
+  fetchData() {
+    return api.getClients()
+      .then((clients) => {
+        this.state = new ClientsState(clients);
+        this.clients = this.state.getClientsInterface().data();
+      });
+  }
+
+  render() {
+    this.table.innerHTML = '';
+    this.table.append(...this.clients.map(createClientRowElement));
+  }
+
+  init() {
+    return this.fetchData().then(() => {
+      this.setEventListeners();
+      this.render();
+    });
+  }
+}
--- project/src/scripts/index.js
+++ project/src/scripts/index.js
@@ -1,50 +1,6 @@
 // import clients from './data/clients';
+import TableApp from './components/table';
 import { clientsTable, tableHead } from './constants';
-import { createClientRowElement } from './elementGenerators';
-import ClientsState from './state/clientsState';
-import api from './api';
 
-class App {
-  constructor(tableElement, sortControls) {
-    this.table = tableElement;
-    this.sortControls = sortControls;
-  }
-
-  setSortControlsEventListeners() {
-    this.sortControls.addEventListener('click', ({ target }) => {
-      if (target.classList.contains('table__control')) {
-        const { sortMethodName } = target.dataset;
-        const studentInterface = this.state.getClientsInterface();
-        this.clients = studentInterface[sortMethodName]().data();
-        this.render();
-      }
-    });
-  }
-
-  setEventListeners() {
-    this.setSortControlsEventListeners();
-  }
-
-  fetchData() {
-    return api.getClients()
-      .then((clients) => {
-        this.state = new ClientsState(clients);
-        this.clients = this.state.getClientsInterface().data();
-      });
-  }
-
-  render() {
-    this.table.innerHTML = '';
-    this.table.append(...this.clients.map(createClientRowElement));
-  }
-
-  init() {
-    return this.fetchData().then(() => {
-      this.setEventListeners();
-      this.render();
-    });
-  }
-}
-
-const app = new App(clientsTable, tableHead);
-app.init();
+const table = new TableApp({ tableElement: clientsTable, sortControls: tableHead, sortControlSelector: 'table__control' });
+table.init();
