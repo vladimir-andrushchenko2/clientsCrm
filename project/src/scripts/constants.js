fd31a4bec12068f5ebc5cb02e643c817d197e5ac
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-01-30 20:13:36 +0100

    added add client button and popup selected elements

--- project/src/scripts/constants.js
+++ project/src/scripts/constants.js
@@ -2,7 +2,14 @@ const clientRowTemplate = document.querySelector('#contact-table-row');
 const clientContactTooltipTemplate = document.querySelector('#contact-tooltip');
 const clientsTable = document.querySelector('.table-clients-container');
 const tableHead = document.querySelector('.table__head');
+const addClientButton = document.querySelector('.add-client-btn');
+const addUserPopUp = document.querySelector('.popup_add-user');
 
 export {
-  clientRowTemplate, clientContactTooltipTemplate, clientsTable, tableHead,
+  clientRowTemplate,
+  clientContactTooltipTemplate,
+  clientsTable,
+  tableHead,
+  addClientButton,
+  addUserPopUp,
 };
